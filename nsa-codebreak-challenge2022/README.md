43efa521040ea7f5ea53c896ae1823e82705c4a5
tuampo <diohabara@gmail.com>
2022-10-18 19:12:59 -0500

    change directory structure

---
+++ nsa-codebreak-challenge2022/README.md
@@ -0,0 +1,11 @@
+# NSA CodeBreaker Challenge 2022
+
+- [A1](./a1.ipynb)
+- [B1](./b1.ipynb)
+
+## References
+
+### A2
+
+- [2021 write-up of network](https://github.com/luker983/nsa-codebreaker-2021/tree/main/task1)
+- [what's ssl/tls](https://www.sslcerts.jp/)
--- nsa-codebreak-challenge2022/bin/convert.sh
+++ nsa-codebreak-challenge2022/bin/convert.sh
@@ -3,6 +3,9 @@
 # Usage: convert.sh
 set -eou pipefail
 for f in *.ipynb; do
-    jupyter nbconvert --to markdown "$f"
+    file=${f%.*}
+    jupyter nbconvert --to markdown "$file"
+    pandoc -s "$file".md -o "docs/$file".pdf
 done
 shfmt -w -i 4 convert.sh
+zip -r solutions docs
